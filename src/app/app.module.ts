11786e03a9d44650fa655187dbac3ced5139d975
Mashudu Ralephata <Mashudu.Ralephata@outlook.com>
2021-04-19 10:46:44 +0200

    Full Angular SignIn/SignUp Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,64 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {AngularFireModule} from '@angular/fire';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+import {firebase, firebaseui, FirebaseUIModule} from 'firebaseui-angular';
+import {environment} from '../environments/environment';
+import {FormsModule} from '@angular/forms';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBCMg814olUKByASl0gage3Dagd8W30olc",
+  authDomain: "contactform-2b661.firebaseapp.com",
+  databaseURL: "https://contactform-2b661-default-rtdb.firebaseio.com",
+  projectId: "contactform-2b661",
+  storageBucket: "contactform-2b661.appspot.com",
+  messagingSenderId: "579728331949",
+  appId: "1:579728331949:web:b3ecc67e65934b5c152224",
+  measurementId: "G-607CJD6WRP"
+};
+
+const firebaseUiAuthConfig: firebaseui.auth.Config = {
+  signInFlow: 'popup',
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    {
+      scopes: [
+        'public_profile',
+        'email',
+        'user_likes',
+        'user_friends'
+      ],
+      customParameters: {
+        'auth_type': 'reauthenticate'
+      },
+      provider: firebase.auth.FacebookAuthProvider.PROVIDER_ID
+    },
+    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    {
+      requireDisplayName: false,
+      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID
+    },
+    firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+    firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+  ],
+  tosUrl: '<your-tos-link>',
+  privacyPolicyUrl: '<your-privacyPolicyUrl-link>',
+  credentialHelper: firebaseui.auth.CredentialHelper.GOOGLE_YOLO
+};
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    FirebaseUIModule.forRoot(firebaseUiAuthConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
